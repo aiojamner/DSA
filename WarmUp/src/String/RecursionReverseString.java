0911c043a8d53877982b5fef3b40f44ca5b83e26
Vikas Naik <aiojamner@gmail.com>
2025-07-13 00:04:27 +0530

    update string programs

--- WarmUp/src/String/RecursionReverseString.java
+++ WarmUp/src/String/RecursionReverseString.java
@@ -2,8 +2,18 @@ package String;
 
 public class RecursionReverseString {
 
+    public static String reverseStringRecursion(String str){
+        if (str == null || str.length() <= 1){
+            return str;
+        }
+
+        return reverseStringRecursion(str.substring(1)) + str.charAt(0);
+    }
 
     public static void main(String[] args) {
 
+        String str = "dsaarmy";
+
+        System.out.println("Reverse string for "+str+ " is "+reverseStringRecursion(str));
     }
 }
